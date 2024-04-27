9e9ddaa730e63a4a1e8962916a2103f93536f5dc
Torin Maguire <torinmaguire@gmail.com>
2024-04-26 21:19:06 -0500

    Clean up and change into library

---
+++ t_read/src/lib.rs
@@ -0,0 +1,20 @@
+use rand::Rng;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+pub fn read(filename: &str) -> Result<String, String> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let split_collection: Vec<Vec<u8>> = reader.split(b'%').map(|l| l.unwrap()).collect();
+    let index = rand::thread_rng().gen_range(0..split_collection.len());
+
+    let result = String::from_utf8(split_collection.get(index).unwrap().to_vec())
+        .unwrap()
+        .trim()
+        .to_string();
+
+    Ok(result)
+}
